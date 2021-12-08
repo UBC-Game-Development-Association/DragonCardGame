f5a3c720468b61f883f7380bf59b55af3b0fd0da
asidhu9 <asidhu994@gmail.com>
2021-12-07 18:08:42 -0800

    changes

--- Assets/Scripts/Game Objects/Card.cs
+++ Assets/Scripts/Game Objects/Card.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using DefaultNamespace;
 using UnityEditor;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.U2D.IK;
 
